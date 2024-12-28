67f111b485589205f7d7199ba045b82c4fa9495b
Junchi Shen <jshen67@jh.edu>
2024-12-28 02:43:07 -0500

    Create script.js

---
+++ Update_2024_12_28/JS_related/script.js
@@ -0,0 +1,16 @@
+
+// This part is to get the files //
+
+// document.getElementById('file-input').addEventListener('change', function(e) {
+//     const file = e.target.files[0];
+//     if (file) {
+//         const reader = new FileReader();
+//         reader.onload = function(e) {
+//             // 获取textarea元素
+//             const textarea = document.querySelector('.search-box textarea');
+//             // 将文件内容放入textarea
+//             textarea.value = e.target.result;
+//         };
+//         reader.readAsText(file);
+//     }
+// });
